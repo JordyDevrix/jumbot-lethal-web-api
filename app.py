2ed27843eb72644217c80debf6ea3d68b5e18452
Jordy Devrix <2109jordy2241@gmail.com>
2024-05-10 00:43:16 +0200

    Changed whole API functionality and started working on authentication

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import random
+
 import discord
 import asyncio
 
 import quart
-from quart import Quart
+from quart import Quart, request
 
 app = Quart(__name__)
 client = discord.Client(intents=discord.Intents.all())
@@ -18,32 +20,71 @@ async def before_serving():
     loop.create_task(client.connect())
 
 
-@app.route("/send/<message>", methods=["GET"])
-async def send_specific_message(message):
-    # wait_until_ready and check for valid connection is missing here
-    channel = client.get_channel(1214700522997678157)
-    await channel.send(message)
-    return 'OK', 200
+@app.route("/login", methods=["POST"])
+async def login():
+    char = "abcdefghijklmnopqrstuvwxyz"
+    btoken = ""
+    for i in range(32):
+        btoken += random.choice((str(random.randint(0, 9)), random.choice(char)))
+    return {"Bearer": btoken}
 
 
-@app.route("/send", methods=["GET"])
-async def send_message():
-    return await quart.render_template("index.html")
+@app.route("/register", methods=["POST"])
+async def register():
+    char = "abcdefghijklmnopqrstuvwxyz"
+    btoken = ""
+    for i in range(32):
+        btoken += random.choice((str(random.randint(0, 9)), random.choice(char)))
+    return {"Bearer": btoken}
 
 
-@app.route("/send", methods=["POST"])
-async def send_message_post():
-    text = await quart.request.form
+@app.route("/guilds", methods=["GET"])
+async def choose_guild():
+    headers = request.headers
+    try:
+        bearer = headers.get('Authorization')
+        btoken = bearer.split()[1]
+        print(btoken)
+    except Exception as e:
+        print(e)
+    client_guild_list = client.guilds
+    guilds = []
+    for guild in client_guild_list:
+        guilds.append(
+            {
+                "guild_id": str(guild.id),
+                "guild_name": guild.name,
+                "guild_member_count": int(len(guild.members))
+            }
+        )
+    print(guilds)
+    return guilds
 
-    title = text['title']
-    content = text['message']
-    message = f"# {title}\n{content}"
-    print(message)
 
-    channel = client.get_channel(1214700522997678157)
-    await channel.send(message)
-
-    return "<p>Message sent</p>"
-
+@app.route("/guild/textchannels/<guild_id>", methods=["GET"])
+async def get_channel_choice(guild_id):
+    headers = request.headers
+    try:
+        bearer = headers.get('Authorization')
+        btoken = bearer.split()[1]
+        print(btoken)
+    except Exception as e:
+        print(e)
+    client_guild_channel_list = client.get_guild(int(guild_id)).channels
+    channels = []
+    for channel in client_guild_channel_list:
+        if channel.type.value == 0:
+            channels.append(
+                {
+                    "channel_id": str(channel.id),
+                    "channel_name": str(
+                        channel.name.encode(
+                            encoding="ascii",
+                            errors="ignore"
+                        )
+                    )[2:-1].replace("-", " ").strip()
+                }
+            )
+    return channels
 
 app.run(host='0.0.0.0', port=5100)
